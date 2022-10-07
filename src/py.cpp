7a5aa1dd6ba43dae99199cfdc11f39b88a54418e
Igor Smirnov <i.smirnov@meldana.com>
2022-10-07 14:19:30 +0500

    Add default parameters and parameter names

--- src/py.cpp
+++ src/py.cpp
@@ -27,9 +27,11 @@ PYBIND11_MODULE(strong_sort_cpp, m) {
     )pbdoc";
 
     py::class_<StrongSort>(m, "StrongSort")
-        .def(py::init<float, float, int, int, int>())
-        .def("increment_ages", &StrongSort::incrementAges)
-        .def("update", &StrongSort::update);
+        .def(py::init<float, float, int, int, int>(),
+             py::arg("max_dist") = 0.2, py::arg("max_iou_distance") = 0.7, py::arg("max_age") = 70, py::arg("n_init") = 3, py::arg("nn_budget") = 100)
+        .def("increment_ages", &StrongSort::incrementAges, "Increment tracks ages, if no detections found")
+        .def("update", &StrongSort::update, "Update tracker state with new detections",
+             py::arg("ltwhs"), py::arg("confidences"), py::arg("classes"), py::arg("features"), py::arg("image_size"));
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
